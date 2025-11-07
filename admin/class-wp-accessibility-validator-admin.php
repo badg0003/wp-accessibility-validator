6abaa6e39f2bf9299ac0fc3572eb79c07fb9eb72
Mike Badgley <mike@digiteam.ca>
2025-11-07 07:49:47 -0500

    Update plugin documentation link in the admin class

--- admin/class-wp-accessibility-validator-admin.php
+++ admin/class-wp-accessibility-validator-admin.php
@@ -2,7 +2,7 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * @link       YOUR_WEBSITE_URL
+ * @link       https://www.digiteam.ca/
  * @since      1.0.0
  *
  * @package    Wp_Accessibility_Validator
