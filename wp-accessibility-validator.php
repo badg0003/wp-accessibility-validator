bbb57c3deb7854f826140fec642cca6f3231c183
Mike Badgley <mike@digiteam.ca>
2025-11-14 16:14:32 -0500

    chore: update version to 2.0.7 in plugin header

--- wp-accessibility-validator.php
+++ wp-accessibility-validator.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       WP Accessibility Validator
  * Description:       Adds an on-demand accessibility checker to the block editor to validate content against WCAG standards.
- * Version:           2.0.1
+ * Version:           2.0.7
  * Author:            Mike Badgley
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
